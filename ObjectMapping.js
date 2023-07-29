8b33ff8ab07453cdb17bebe51c8bc8a17b6d218d
JhaAakri <aditijha0830@gmail.com>
2023-07-29 16:43:03 +0530

    ObjectMapping

---
+++ ObjectMapping.js
@@ -0,0 +1,8 @@
+let users = [
+    { firstName: "Nrupul", lastName: "Dev", place: "Banglore" },
+    { firstName: "Prateek", lastName: "Shukla", place: "Banglore" },
+    { firstName: "Yogesh", lastName: "Bhat", place: "Kolkata" },
+  ];
+  const fullNameArray = users.map((user) => `${user.firstName} ${user.lastName}`);
+  console.log(fullNameArray);
+  
